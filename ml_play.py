96ddb5a9ce35a2488fe6934b9a0db09e0ee8ca96
zoyam-chuang <55427645+zoyam-chuang@users.noreply.github.com>
2020-07-28 11:14:25 +0800

    Add files via upload

---
+++ ml_play.py
@@ -0,0 +1,36 @@
+"""
+The template of the main script of the machine learning process
+"""
+
+class MLPlay:
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.ball_served = False
+
+    def update(self, scene_info):
+        """
+        Generate the command according to the received `scene_info`.
+        """
+        # Make the caller to invoke `reset()` for the next round.
+        if (scene_info["status"] == "GAME_OVER" or
+            scene_info["status"] == "GAME_PASS"):
+            return "RESET"
+        #print(scene_info["ball"])
+        if not self.ball_served:
+            self.ball_served = True
+            command = "SERVE_TO_LEFT"
+        elif scene_info["ball"][0] > 75:
+            command = "MOVE_RIGHT"
+        #    print(scene_info["ball"][0])
+        else:
+            command = "MOVE_LEFT"
+
+        return command
+
+    def reset(self):
+        """
+        Reset the status
+        """
+        self.ball_served = False
